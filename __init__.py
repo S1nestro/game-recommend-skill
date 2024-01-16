3567a9bace30e86dfa3e5ed7e49eab8fb89cb460
S1nestro <90309994+S1nestro@users.noreply.github.com>
2024-01-16 15:35:37 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,21 @@
 from mycroft import MycroftSkill, intent_handler
-
+from adapt.intent import IntentBuilder
 
 class GameRecommend(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.games_list = ["The Witcher 3", "Stardew Valley", "Celeste", "Hollow Knight", "Minecraft"]
 
     @intent_handler('recommend.game.intent')
     def handle_recommend_game(self, message):
-        recommended_game = choice(self.games_list)
-        self.speak_dialog('recommend.game', {'game': recommended_game})
+        self.speak_dialog('recommend.game')
 
+    @intent_handler(IntentBuilder('HowToUseIntent').require('HowToUseKeyword').require('SkillKeyword'))
+    def handle_how_to_use(self, message):
+        self.speak("You can use this skill by asking me to recommend a game.")
 
+    @intent_handler(IntentBuilder('WhatCanDoIntent').require('FunctionalityKeyword').require('SkillKeyword'))
+    def handle_what_can_do(self, message):
+        self.speak("I can recommend a random game from a predefined list.")
 
 def create_skill():
     return GameRecommend()
-
