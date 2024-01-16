388ba805fb3069ad26b49ada8b31daba589c1a4d
S1nestro <90309994+S1nestro@users.noreply.github.com>
2024-01-16 15:37:44 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,9 @@ from adapt.intent import IntentBuilder
 class GameRecommend(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
+        
+    # Adapt
+    
     @intent_handler('recommend.game.intent')
     def handle_recommend_game(self, message):
         self.speak_dialog('recommend.game')
@@ -15,6 +17,16 @@ class GameRecommend(MycroftSkill):
 
     @intent_handler(IntentBuilder('WhatCanDoIntent').require('FunctionalityKeyword').require('SkillKeyword'))
     def handle_what_can_do(self, message):
+        self.speak("I can recommend a random game from a predefined list.")
+
+    # Padatious
+    
+    @intent_file_handler('how_to_use.intent')
+    def handle_how_to_use_padatious(self, message):
+        self.speak("You can use this skill by asking me to recommend a game.")
+
+    @intent_file_handler('what_can_do.intent')
+    def handle_what_can_do_padatious(self, message):
         self.speak("I can recommend a random game from a predefined list.")
 
 def create_skill():
