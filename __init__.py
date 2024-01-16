263c863d43e70a87db24a71f252828a3f466006c
S1nestro <90309994+S1nestro@users.noreply.github.com>
2024-01-16 15:25:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,13 @@ from mycroft import MycroftSkill, intent_handler
 class GameRecommend(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.games_list = ["The Witcher 3", "Stardew Valley", "Celeste", "Hollow Knight", "Minecraft"]
 
     @intent_handler('recommend.game.intent')
     def handle_recommend_game(self, message):
-        self.speak_dialog('recommend.game')
+        recommended_game = choice(self.games_list)
+        self.speak_dialog('recommend.game', {'game': recommended_game})
+
 
 
 def create_skill():
